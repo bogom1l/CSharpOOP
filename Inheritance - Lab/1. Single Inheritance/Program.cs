1d6e3f6279ffa1416ecba095ff8ff7bb290f19f2
bogi <bogity123@gmail.com>
2022-06-30 18:42:55 +0300

    First Lab

---
+++ Inheritance - Lab/1. Single Inheritance/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace _1._Single_Inheritance
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Worlasddsdsasadd!");
+            Console.WriteLine(1);
+
+
+
+
+
+
+        }
+    }
+}
