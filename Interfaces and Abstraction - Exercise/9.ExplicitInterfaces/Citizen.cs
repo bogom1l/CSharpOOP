5fb5cfc4890a3b67befbef7a2a266fc7a66153cc
bogi <bogity123@gmail.com>
2022-07-11 18:14:32 +0300

    pushed all left todo tasks

---
+++ Interfaces and Abstraction - Exercise/9.ExplicitInterfaces/Citizen.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _9.ExplicitInterfaces
+{
+    public class Citizen : IResident, IPerson
+    {
+        private string name;
+        private string country;
+        private int age;
+
+        public Citizen(string n, string c, int a)
+        {
+            this.Name = n;
+            this.Age = a;
+            this.Country = c;
+        }
+        public string Name { get; set; }
+
+        public string Country { get; set; }
+
+        public int Age { get; set; }
+
+        string IResident.GetName()
+        {
+            return $"Mr/Ms/Mrs {this.Name}";
+        }
+
+        string IPerson.GetName()
+        {
+            return this.Name;
+        }
+
+    }
+}
---
+++ Interfaces and Abstraction - Exercise/9.ExplicitInterfaces/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace _9.ExplicitInterfaces
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                string cmd = Console.ReadLine();
+                if (cmd == "End")
+                {
+                    break;
+                }
+
+                string[] tokens = cmd.Split();
+                string name = tokens[0];
+                string country = tokens[1];
+                int age = int.Parse(tokens[2]);
+                
+                IPerson person = new Citizen(name, country, age);
+                IResident resident = new Citizen(name, country, age);
+
+                Console.WriteLine(person.GetName());
+                Console.WriteLine(resident.GetName());
+            }
+        }
+    }
+}
