f00f24ef80be12650c44455ca027507ed42db4da
bogi <bogity123@gmail.com>
2022-07-12 11:01:13 +0300

    pushed but will edit l8r

---
+++ Encapsulation - Exercise/4.PizzaCalories_v2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _4.PizzaCalories_v2
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello WorASDASDASDSDSld!");
+        }
+    }
+}
