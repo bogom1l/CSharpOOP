8d0c74ffd1a2b720f758495abaf2897e214e04e2
bogi <bogity123@gmail.com>
2022-07-12 16:11:23 +0300

    awesome done

---
+++ Encapsulation - Exercise/4.PizzaCalories_v2/PizzaCalories/Dough.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PizzaCalories
+{
+    public class Dough
+    {
+        private const int BaseCaloriesPerGram = 2;
+
+        private List<string> AllValidFlourTypes = new List<string>()
+        {
+            "white", "wholegrain"
+        };
+
+        private List<string> AllValidBakingTechniques = new List<string>()
+        {
+            "crispy", "chewy", "homemade"
+        };
+
+        private string flourType; //white or wholegrain
+        private string bakingTechnique; // crispy, chewy, or homemade
+        private double weight;
+
+        public Dough(string flourType, string bakingTechnique, double weight)
+        {
+            this.FlourType = flourType;
+            this.BakingTechnique = bakingTechnique;
+            this.Weight = weight;
+        }
+
+        public string FlourType
+        {
+            get => this.flourType;
+            private set
+            {
+                if (!AllValidFlourTypes.Contains(value.ToLower()))
+                {
+                    throw new Exception("Invalid type of dough.");
+                }
+                this.flourType = value; 
+            }
+        }
+        public string BakingTechnique
+        {
+            get => this.bakingTechnique;
+            private set
+            {
+                if (!AllValidBakingTechniques.Contains(value.ToLower()))
+                {
+                    throw new Exception("Invalid type of dough.");
+                }
+                this.bakingTechnique = value; 
+            }
+        }
+        public double Weight 
+        {
+            get => this.weight;
+            private set
+            {
+                if (value < 1 || value > 200)
+                {
+                    throw new Exception("Dough weight should be in the range [1..200].");
+                }
+                this.weight = value;
+            }
+        }
+
+        private double CalculateCalories()
+        {
+            double modifier = BaseCaloriesPerGram;
+
+            if (this.FlourType.ToLower() == "white") 
+            {
+                modifier *= 1.5;
+            }
+            else if(this.FlourType.ToLower() == "wholegrain")
+            {
+                modifier *= 1.0;
+            }
+            
+            if(this.BakingTechnique.ToLower() == "crispy")
+            {
+                modifier *= 0.9;
+            }
+            else if(this.BakingTechnique.ToLower() == "chewy")
+            {
+                modifier *= 1.1;
+            }
+            else if(this.BakingTechnique.ToLower() == "homemade")
+            {
+                modifier *= 1.0;
+            }
+
+            return modifier * this.Weight; //(modifier = calories per Gram => this.Weight * modifier = total calories) ;
+        }
+
+        public double GetCalories => this.CalculateCalories();
+
+
+    }
+}
---
+++ Encapsulation - Exercise/4.PizzaCalories_v2/PizzaCalories/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace PizzaCalories
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                string command1 = Console.ReadLine();
+                string[] tokensPizza = command1.Split();
+                string pizzaName = tokensPizza[1];
+                //Pizza pizza = new Pizza(tokensPizza[0]);
+
+                string command2 = Console.ReadLine();
+                string[] tokensDough = command2.Split();
+                string currDoughFlourType = tokensDough[1];
+                string currDoughBT = tokensDough[2];
+                double currDoughWeight = double.Parse(tokensDough[3]);
+                Dough dough = new Dough(currDoughFlourType, currDoughBT, currDoughWeight);
+
+                Pizza pizza = new Pizza(pizzaName, dough);
+
+                while (true)
+                {
+                    string inputTopping = Console.ReadLine();
+                    if (inputTopping == "END")
+                    {
+                        break;
+                    }
+
+                    string[] tokens = inputTopping.Split();
+                    string currType = tokens[1];
+                    double currWeight = double.Parse(tokens[2]);
+                    Topping currTopping = new Topping(currType, currWeight);
+
+                    pizza.AddTopping(currTopping);
+
+                }
+
+                Console.WriteLine($"{pizza.Name} - {pizza.GetTotalCalories:f2} Calories.");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+        }
+    }
+}
