ba9a880abc4ca1746f3b01ab2f4af3a003d9e208
wavezM5 <102171820+wavezM5@users.noreply.github.com>
2022-08-12 14:13:36 +0300

    pushed 2nd variant of exam solution

---
+++ Exams/C# OOP Retake Exam - 20 Dec 2021/3/2nd variant, idk maybe better/BookSkeleton/Book.Tests/Tests.cs
@@ -0,0 +1,72 @@
+﻿using System.Numerics;
+
+namespace Book.Tests
+{
+    using System;
+
+    using NUnit.Framework;
+
+    public class Tests
+    {
+        [Test]
+        public void TestConstructor()
+        {
+            Book book = new Book("Im so sorry", "Me");
+
+            Assert.AreEqual(0, book.FootnoteCount);
+            Assert.AreEqual("Im so sorry", book.BookName);
+            Assert.AreEqual("Me", book.Author);
+        }
+
+        [Test]
+        public void BookNameError()
+        {
+            Assert.Throws<ArgumentException>(() => new Book(null, "Me"));
+        }
+
+        [Test]
+        public void AuthorError()
+        {
+            Assert.Throws<ArgumentException>(() => new Book("Im so sorry", null));
+        }
+
+        [Test]
+        public void TestAddFootnote()
+        {
+            Book book = new Book("Im so sorry", "Me");
+            book.AddFootnote(1, "Angry");
+            Assert.AreEqual(1, book.FootnoteCount);
+
+            book.AddFootnote(50, "Sad");
+            book.AddFootnote(25, "Happy");
+            Assert.AreEqual(3, book.FootnoteCount);
+
+            Assert.Throws<InvalidOperationException>(() => book.AddFootnote(1, "Angry"));
+        }
+
+        [Test]
+        public void TestFindFootnote()
+        {
+            Book book = new Book("Im so sorry", "Me");
+            book.AddFootnote(1, "Angry");
+            Assert.Throws<InvalidOperationException>(() => book.FindFootnote(25));
+
+            Assert.AreEqual("Footnote #1: Angry", book.FindFootnote(1));
+        }
+
+        [Test]
+        public void TestAlterFootnote()
+        {
+            Book book = new Book("Im so sorry", "Me");
+            book.AddFootnote(1, "Angry");
+            Assert.Throws<InvalidOperationException>(() => book.AlterFootnote(25, "Its ok"));
+
+            book.AddFootnote(123, "qwe");
+            book.AlterFootnote(123, "newMessage");
+
+            Assert.AreEqual($"Footnote #{123}: {"newMessage"}", book.FindFootnote(123));
+        }
+
+
+    }
+}
